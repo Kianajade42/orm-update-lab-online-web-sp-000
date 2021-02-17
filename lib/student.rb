62a46fbeeb6cd70fc20f2648cfafa205353e26f0
Kiana Long <kianajade42.kl@gmail.com>
2021-02-17 19:58:33 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -4,6 +4,38 @@ class Student
 
   # Remember, you can access your database connection anywhere in this class
   #  with DB[:conn]
-
+def initialize(name,grade,id=nil)
+  @id=id 
+  @name=name
+  @grade=grade
 
 end
+def .create_table
+  CREATE TABLE students (
+id INTEGER PRIMARY KEY 
+name TEXT 
+grade TEXT
+  
+  )
+end
+  def .drop_table
+    
+  end
+  def save
+    
+  end
+   def .create
+student=
+   end
+
+   def .new_from_db(row)
+
+   end
+
+   def find_by_name
+     
+   end
+
+   def update
+
+   end
